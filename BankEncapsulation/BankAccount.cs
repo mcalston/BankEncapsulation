80ed93a01f4a4c3eabcf8929a10052f78c24bb05
Miles Alston <milescalston@gmail.com>
2020-11-06 21:28:26 -0500

    initial commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+public class BankAccount
+    {
+        private double balance;
+
+        //public BankAccount(double amount)
+        //{
+        //    balance = amount;
+        //}
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Simulating a deposit of {amount,0:c} to your account...");
+            balance = amount;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+    
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount ba = new BankAccount();
+
+            Console.WriteLine("How much $ would you like to deposit?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            ba.Deposit(amountToDeposit);
+
+            double userBalance = ba.GetBalance();
+
+            Console.WriteLine($"Your current balace is: {userBalance,0:c}");
+
+            
+                
+        }
+    }
+}
